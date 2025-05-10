6973e1b15b206506dec5a9a5eae2885f9455e7f3
talachan <talachan@yandex.ru>
2025-05-10 23:57:37 +0300

    Фикс наследуемых классов meat

--- src/main/java/model/Apple.java
+++ src/main/java/model/Apple.java
@@ -2,7 +2,8 @@ package model;
 
 public abstract class Apple extends Food {
     private String colour;
-    public Apple (int price, double amount, boolean isVegetarian) {
+    public Apple (int price, double amount, boolean isVegetarian, String colour) {
         super(price, amount, isVegetarian);
+        this.colour = colour;
     }
 }
