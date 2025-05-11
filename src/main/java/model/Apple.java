c841fea74e3b608d552c5a542bc35b60da23216b
talachan <talachan@yandex.ru>
2025-05-11 21:49:59 +0300

    Добавить констатны colour в main

--- src/main/java/model/Apple.java
+++ src/main/java/model/Apple.java
@@ -1,13 +1,21 @@
 package model;
-import static model.constants.Colour.RED;
-import static model.constants.Colour.GREEN;
 
 
 public class Apple extends Food {
     private final String colour;
-    public Apple(int price, int amount, boolean isVegetarian, String colour) {
+    private Apple(int price,
+                  int amount,
+                  boolean isVegetarian,
+                  String colour) {
         super(price, amount, isVegetarian);
         this.colour = colour;
+    }
+
+    public static Apple newApple(int price,
+                                 int amount,
+                                 boolean isVegetarian,
+                                 String colour) {
+        return new Apple(price, amount, isVegetarian, colour);
     }
 
 
--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -1,12 +1,13 @@
 package model;
 
-import model.service.ShoppingCart;
+import static model.constants.Colour.GREEN;
+import static model.constants.Colour.RED;
 
 public abstract class Main {
     public static void main(String[] args) {
         Meat meat = new Meat(100,5,false);
-        Apple greenApple = new Apple(50, 10, true);
-        Apple redApple = new Apple(60,8, true);
+        Apple greenApple = Apple.newApple(50, 10, true, RED);
+        Apple redApple = Apple.newApple(60,8, true, GREEN);
 
 
     }
